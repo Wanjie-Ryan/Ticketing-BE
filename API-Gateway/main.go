1bff533f0ed328ce082da12aaab27eb8bb05d35e
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2025-10-31 20:44:52 +0300

    gateway design

--- API-Gateway/main.go
+++ API-Gateway/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	// "fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	echomw "github.com/labstack/echo/v4/middleware"
+)
 
 func main() {
-	fmt.Println("API Gateway is running...")
+
+	e := echo.New()
+
+	e.Use(echomw.CORSWithConfig(echomw.CORSConfig{
+		AllowOrigins: []string{
+			"http://localhost:3000",
+			"http://localhost:3001",
+		},
+		AllowHeaders: []string{
+			echo.HeaderOrigin,
+			echo.HeaderContentType,
+			echo.HeaderAccept,
+			echo.HeaderAuthorization,
+		},
+		AllowMethods: []string{
+			http.MethodGet,
+			http.MethodPost,
+			http.MethodPatch,
+			http.MethodPut,
+			http.MethodDelete,
+		},
+	}))
+
+	e.Use(echomw.RateLimiter(
+		echomw.NewRateLimiterMemoryStore(5),
+	))
+	
 }
