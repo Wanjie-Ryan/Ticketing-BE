2fd8a675766d41470f50b52d91a99f4d370a9f62
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2025-10-31 21:17:09 +0300

    gateway design

--- API-Gateway/main.go
+++ API-Gateway/main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
-	// "fmt"
+	"context"
+	"fmt"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	echomw "github.com/labstack/echo/v4/middleware"
+	"github.com/sirupsen/logrus"
 )
 
+// init functions are executed before main, and is mostly used for package initialization like setting up loggers, initializing configurations, DB connections, etc.
+
+func init() {
+	logrus.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp:   true,
+		ForceColors:     true,
+		TimestampFormat: "2006-01-02 15:04:05",
+	})
+	logrus.SetLevel(logrus.InfoLevel)
+}
+
 func main() {
+
+	ctx := context.Background()
+
+	logrus.WithContext(ctx).Info("Starting API Gateway...")
 
 	e := echo.New()
 
@@ -35,5 +54,21 @@ func main() {
 	e.Use(echomw.RateLimiter(
 		echomw.NewRateLimiterMemoryStore(5),
 	))
-	
+
+	err := godotenv.Load()
+
+	if err != nil {
+		logrus.WithContext(ctx).Fatalf("Error loading .env file: %v", err)
+	}
+
+	e.GET("/health-check", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Server is healthy")
+	})
+
+	port := os.Getenv("APP_PORT")
+	appAddress := fmt.Sprintf(":%s", port)
+	// fmt.Println("Server is running on port:", port)
+	logrus.WithContext(ctx).Infof("API Gateway started on %s", appAddress)
+	e.Logger.Fatal(e.Start(appAddress))
+
 }
